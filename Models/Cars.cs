7f433b6dd530248248de76704051445223ac2f8a
Fahad <fahadmiq1@gmail.com>
2023-11-22 11:14:09 +0500

    Add files via upload

---
+++ Models/Cars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCars.Models
+{
+    public class Cars
+    {
+        public int AutomobileID { get; set; }
+        public int CompanyID { get; set; }
+        public string Status { get; set; }
+        public int ModelID { get; set; }
+        public double Price { get; set; }
+
+        // getting Company Name from Companies Table
+        public string Name { get; set; }
+
+        // getting Columns from Model Details Table
+        public int ModelNumber { get; set; }
+        public string ModelName { get; set; }
+        public int ModelYear { get; set; }
+    }
+}
---
+++ Models/UserRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyCars.Models
+{
+    public class UserRegistration
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
